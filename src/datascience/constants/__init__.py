ba09b967e041027ff84bba28e8b0a966ce1f9528
lokesh <lokesh@MacBook-Air-2.local>
2025-07-29 16:04:54 +0530

    completed data ingestion

---
+++ src/datascience/constants/__init__.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH=Path("/Users/lokesh/Desktop/MLFLOW/datascienceproject/config/config.yaml")
+PARAMS_FILE_PATH = Path("/Users/lokesh/Desktop/MLFLOW/datascienceproject/params.yaml")
+SCHEMA_FILE_PATH = Path("/Users/lokesh/Desktop/MLFLOW/datascienceproject/schema.yaml")
